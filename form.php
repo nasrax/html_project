a0499bd1619d90a7c139007833a79ba760f35701
iambaraka <barakamulumia@gmail.com>
2020-09-01 16:58:24 +0300

    Nothing added just Indentations

--- form.php
+++ form.php
@@ -1,24 +1,27 @@
  <html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Add Record Form</title>
-</head>
-<body>
-<form action="hello.php" method="post">
-<p>
-<label for="firstName">First Name:</label>
 
-<input type="text" name="first_name" id="firstName">
-</p>
-<p>
-<label for="lastName">Last Name:</label>
-<input type="text" name="last_name" id="lastName">
-</p>
-<p>
-<label for="emailAddress">Email Address:</label>
-<input type="text" name="email" id="emailAddress">
-</p>
-<input type="submit" value="Submit">
-</form>
-</body>
-</html>
+ <head>
+    <meta charset="UTF-8">
+    <title>Add Record Form</title>
+ </head>
+
+ <body>
+    <form action="hello.php" method="post">
+       <p>
+          <label for="firstName">First Name:</label>
+
+          <input type="text" name="first_name" id="firstName">
+       </p>
+       <p>
+          <label for="lastName">Last Name:</label>
+          <input type="text" name="last_name" id="lastName">
+       </p>
+       <p>
+          <label for="emailAddress">Email Address:</label>
+          <input type="text" name="email" id="emailAddress">
+       </p>
+       <input type="submit" value="Submit">
+    </form>
+ </body>
+
+ </html>
